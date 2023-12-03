b89c420427d47eca5bb81f91ab56ef5578629fc2
prasadn-16 <nagthanep@gmail.com>
2023-12-04 00:07:04 +0530

    end of inception

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 const heading = React.createElement(
   "h1",
-  { id: "heading", },
+  { id: "heading" },
   "Hello World from React"
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+{
+  /* <div id="Parent">
+    <div id="child">
+    <h1>I am H1 Tag</h1>
+    <h2>I am H1 Tag</h2>
+    </div>
+    </div> */
+}
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 Tag"),
+    React.createElement("h2", {}, "I am H2 Tag"),
+  ])
+);
+
+const twoSibling = [React.createElement("h1")];
+console.log(parent);
 root.render(heading);
-    
+root.render(parent);
